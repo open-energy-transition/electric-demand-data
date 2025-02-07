bbaee9d93033efb4321cca3c7ca9829c9f953fff
eantonini <50218270+eantonini@users.noreply.github.com>
2025-02-07 16:15:57 +0000

    Changed file organization and implemented suggested changes

---
+++ scripts/download_weather_data_from_Copernicus.py
@@ -0,0 +1,329 @@
+# Source: https://cds.climate.copernicus.eu/how-to-api
+
+import logging
+import os
+
+import cdsapi
+import pandas as pd
+import pytz
+import util.general_utilities as general_utilities
+import util.geospatial_utilities as geospatial_utilities
+import xarray as xr
+
+
+def get_request(
+    ERA5_variable: str,
+    year: int,
+    region_bounds: list[float] = None,
+    extra_time_steps: pd.DatetimeIndex = None,
+    uneven_utc_offset: bool = False,
+) -> dict[str, str | list[str]]:
+    """
+    Get the request for the ERA5 data from the Copernicus Climate Data Store (CDS).
+
+    Parameters
+    ----------
+    ERA5_variable : str
+        The ERA5 variable of interest
+    year : int
+        The year of the data retrieval
+    region_bounds : list of float, optional
+        The lateral bounds of the region of interest (West, South, East, North)
+    extra_time_steps : pandas.DatetimeIndex, optional
+        The extra time steps to consider
+    uneven_utc_offset : bool, optional
+        Whether the UTC offset is uneven
+
+    Returns
+    -------
+    request : dict of str or list of str
+        The request for the ERA5 data
+    """
+
+    # Initialize the request with the common parameters.
+    request = {
+        "product_type": ["reanalysis"],
+        "variable": [ERA5_variable],
+        "data_format": "netcdf",
+        "download_format": "unarchived",
+    }
+
+    # Add the region bounds if they are provided.
+    if region_bounds is not None:
+        request["area"] = [
+            region_bounds[3],
+            region_bounds[0],
+            region_bounds[1],
+            region_bounds[2],
+        ]  # North, West, South, East
+
+    # Add the time steps of the year of interest or the extra time steps.
+    if extra_time_steps is None:
+        request["year"] = [str(int(year))]
+        request["month"] = [
+            str(int(mm)) if mm >= 10 else "0" + str(int(mm)) for mm in range(1, 13)
+        ]
+        request["day"] = [
+            str(int(dd)) if dd >= 10 else "0" + str(int(dd)) for dd in range(1, 32)
+        ]
+        request["time"] = [
+            str(int(tt)) + ":00" if tt >= 10 else "0" + str(int(tt)) + ":00"
+            for tt in range(0, 24)
+        ]
+    else:
+        request["year"] = [str(int(extra_time_steps.year[0]))]
+        request["month"] = [
+            str(int(extra_time_steps.month[0]))
+            if extra_time_steps.month[0] >= 10
+            else "0" + str(int(extra_time_steps.month[0]))
+        ]
+        request["day"] = [
+            str(int(extra_time_steps.day[0]))
+            if extra_time_steps.day[0] >= 10
+            else "0" + str(int(extra_time_steps.day[0]))
+        ]
+        if uneven_utc_offset:
+            # Add an additional time step to consider the uneven UTC offset.
+            # If the additional time steps are in the following year and are the first time steps of the year, add an additional time step at the end of the time steps.
+            if extra_time_steps.hour[0] == 0:
+                extra_time_steps = extra_time_steps.union(
+                    pd.DatetimeIndex([extra_time_steps[-1] + pd.Timedelta(hours=1)])
+                )
+            # If the additional time steps are in the previous year and are the last time steps of the year, add an additional time step at the beginning of the time steps.
+            elif extra_time_steps.hour[-1] == 23:
+                extra_time_steps = extra_time_steps.union(
+                    pd.DatetimeIndex([extra_time_steps[0] - pd.Timedelta(hours=1)])
+                )
+        request["time"] = [
+            str(int(tt)) + ":00" if tt >= 10 else "0" + str(int(tt)) + ":00"
+            for tt in extra_time_steps.hour
+        ]
+
+    return request
+
+
+def merge_temporary_temperature_datasets(
+    file_path_1: str,
+    file_path_2: str,
+    file_path_final: str,
+    time_steps: pd.DatetimeIndex,
+    uneven_utc_offset: bool,
+) -> None:
+    """
+    Merge two temporary temperature datasets.
+
+    Parameters
+    ----------
+    file_path_1 : str
+        The file path of the first temperature dataset
+    file_path_2 : str
+        The file path of the second temperature dataset
+    file_path_final : str
+        The file path of the final temperature dataset
+    time_steps : pandas.DatetimeIndex
+        The time steps of the temperature to retain
+    uneven_utc_offset : bool
+        Whether the UTC offset is uneven
+    """
+
+    # Load the two temperature datasets.
+    temperature_data_1 = xr.open_dataarray(file_path_1)
+    temperature_data_2 = xr.open_dataarray(file_path_2)
+
+    # Merge the two temperature datasets.
+    temperature_data = xr.concat(
+        [temperature_data_1, temperature_data_2], dim="valid_time"
+    )
+    temperature_data = temperature_data.sortby("valid_time")
+
+    if uneven_utc_offset:
+        # Interpolate the temperature data at the provided time steps that are on whole hours.
+        temperature_data = temperature_data.interp(
+            valid_time=time_steps, method="linear"
+        )
+    else:
+        # Keep only the time steps of the year of interest in the local time zone.
+        temperature_data = temperature_data.sel(valid_time=time_steps)
+
+    # Save the final temperature dataset.
+    temperature_data.to_netcdf(file_path_final)
+
+    # Remove the temporary temperature datasets.
+    os.remove(file_path_1)
+    os.remove(file_path_2)
+
+
+def download_ERA5_data_from_Copernicus(
+    year: int,
+    ERA5_variable: str,
+    file_path: str,
+    region_bounds: list[float] = None,
+    local_time_zone: pytz.timezone = None,
+) -> None:
+    """
+    Download the ERA5 data from the Copernicus Climate Data Store (CDS).
+
+    Parameters
+    ----------
+    year : int
+        The year of the data retrieval
+    ERA5_variable : str
+        The ERA5 variable of interest
+    file_path : str
+        The full file path to store the ERA5 data
+    region_bounds : list of float, optional
+        The lateral bounds of the region of interest (West, South, East, North)
+    local_time_zone : pytz.timezone, optional
+        The time zone of the region of interest
+    """
+
+    # Create a new CDS API client.
+    client = cdsapi.Client()
+
+    # Define the dataset.
+    dataset = "reanalysis-era5-single-levels"
+
+    if local_time_zone is None:
+        # Define the request.
+        request = get_request(ERA5_variable, year, region_bounds)
+        client.retrieve(dataset, request, file_path)
+
+    else:
+        # Get all the time steps of the year in the time zone of the country and convert them to UTC.
+        time_steps = (
+            (
+                pd.date_range(
+                    start=f"{year}-01-01",
+                    end=f"{year+1}-01-01",
+                    freq="h",
+                    tz=local_time_zone,
+                )[:-1]
+            )
+            .tz_convert("UTC")
+            .tz_convert(None)
+        )
+
+        # Find the time steps that are not in the year of interest (UTC) when considering the time zone of the country.
+        extra_time_steps = time_steps[time_steps.year != year]
+
+        # Get the time difference to check if the time zone of the country has an uneven UTC offset.
+        time_difference = general_utilities.calculate_time_difference(local_time_zone)
+
+        # If the time difference (in hours) is not an integer number of hours, the UTC offset is uneven.
+        uneven_utc_offset = time_difference % 1 != 0
+
+        if len(extra_time_steps) > 0:
+            # Define the temporary file paths.
+            temporaty_file_path_1 = file_path.replace(".nc", "__temporary_1.nc")
+            temporaty_file_path_2 = file_path.replace(".nc", "__temporary_2.nc")
+
+            # Define two requests: one for the year of interest and one for the extra time steps.
+            request_1 = get_request(
+                ERA5_variable,
+                year,
+                region_bounds,
+                extra_time_steps=extra_time_steps,
+                uneven_utc_offset=uneven_utc_offset,
+            )
+            request_2 = get_request(ERA5_variable, year, region_bounds)
+
+            # Download the two temperature datasets.
+            client.retrieve(dataset, request_1, temporaty_file_path_1)
+            client.retrieve(dataset, request_2, temporaty_file_path_2)
+
+            # Merge the two temperature datasets.
+            merge_temporary_temperature_datasets(
+                temporaty_file_path_1,
+                temporaty_file_path_2,
+                file_path,
+                time_steps,
+                uneven_utc_offset,
+            )
+
+        else:
+            # Define the request.
+            request = get_request(ERA5_variable, year, region_bounds)
+            client.retrieve(dataset, request, file_path)
+
+
+def run_weather_data_retrieval() -> None:
+    """
+    Run the weather data retrieval from the Copernicus Climate Data Store (CDS).
+    """
+
+    # Set up the logging configuration.
+    log_files_directory = general_utilities.read_folders_structure()["log_files_folder"]
+    log_file_name = "weather_data_from_Copernicus.log"
+    logging.basicConfig(
+        filename=log_files_directory + "/" + log_file_name,
+        level=logging.INFO,
+        filemode="w",
+        format="%(asctime)s - %(levelname)s - %(message)s",
+    )
+
+    # Create a directory to store the weather data.
+    result_directory = general_utilities.read_folders_structure()["weather_data_folder"]
+    os.makedirs(result_directory, exist_ok=True)
+
+    # Read the ISO Alpha-2 codes of the countries of interest.
+    settings_directory = general_utilities.read_folders_structure()["settings_folder"]
+    country_codes = general_utilities.read_countries_from_file(
+        settings_directory + "/gegis__all_countries.txt"
+    )
+
+    # Define the ERA5 variables to download.
+    era5_variables = ["2m_temperature"]
+
+    # Define the years of the data retrieval.
+    start_year = 2015
+    end_year = 2015
+    years = range(start_year, end_year + 1)
+
+    # Loop over the years.
+    for year in years:
+        logging.info(f"Year {year}.")
+
+        # Loop over the countries.
+        for country_code in country_codes:
+            logging.info(f"Retrieving data for {country_code}.")
+
+            # Loop over the ERA5 variables.
+            for era5_variable in era5_variables:
+                logging.info(f"Retrieving {era5_variable} data...")
+
+                # Define the full file path of the ERA5 data.
+                era5_data_file_path = (
+                    result_directory + f"/{era5_variable}_{country_code}_{year}.nc"
+                )
+
+                # Check if the file does not exist.
+                if not os.path.exists(era5_data_file_path):
+                    # Get the region of interest.
+                    region_shape = geospatial_utilities.get_geopandas_region(
+                        country_code
+                    )
+
+                    # Get the lateral bounds of the region of interest.
+                    region_bounds = geospatial_utilities.get_region_bounds(
+                        region_shape
+                    )  # West, South, East, North
+
+                    # Get the time zone of the country.
+                    country_time_zone = general_utilities.get_time_zone(
+                        country_code=country_code
+                    )
+
+                    # Download the ERA5 data from the Copernicus Climate Data Store (CDS).
+                    download_ERA5_data_from_Copernicus(
+                        year,
+                        era5_variable,
+                        era5_data_file_path,
+                        region_bounds=region_bounds,
+                        local_time_zone=country_time_zone,
+                    )
+
+                    logging.info(f"Downloaded {era5_variable} data.")
+
+
+if __name__ == "__main__":
+    run_weather_data_retrieval()
---
+++ scripts/util/geospatial_utilities.py
@@ -0,0 +1,290 @@
+import logging
+
+import atlite
+import cartopy.io.shapereader as shpreader
+import geopandas as gpd
+import numpy as np
+import pandas as pd
+import pycountry
+import util.figure_utilities as figure_utilities
+import xarray as xr
+from shapely.geometry import Polygon
+
+
+def remove_remote_islands(
+    region_shape: gpd.GeoDataFrame, country_code: str
+) -> gpd.GeoDataFrame:
+    """
+    Remove small remote islands from the shape of some countries.
+
+    Parameters
+    ----------
+    region_shape : geopandas.GeoDataFrame
+        GeoDataFrame containing the region of interest
+    country_code : str
+        The ISO Alpha-2 code of the country of interest
+
+    Returns
+    -------
+    region_shape : geopandas.GeoDataFrame
+        GeoDataFrame containing the region of interest without small remote islands
+    """
+
+    if country_code in ["CL", "ES", "FR", "NL", "NO", "NZ", "PT"]:
+        # Create a GeoDataFrame to exclude remote islands.
+        if country_code == "CL":  # Chile
+            new_bounds = gpd.GeoSeries(
+                Polygon([(-80, -60), (-60, -60), (-60, 0), (-80, 0)])
+            )
+        elif country_code == "ES":  # Spain
+            new_bounds = gpd.GeoSeries(
+                Polygon([(-10, 35), (5, 35), (5, 45), (-10, 45)])
+            )
+        elif country_code == "FR":  # France
+            new_bounds = gpd.GeoSeries(
+                Polygon([(-5, 40), (10, 40), (10, 55), (-5, 55)])
+            )
+        elif country_code == "NL":  # Netherlands
+            new_bounds = gpd.GeoSeries(Polygon([(0, 50), (10, 50), (10, 55), (0, 55)]))
+        elif country_code == "NO":  # Norway
+            new_bounds = gpd.GeoSeries(Polygon([(0, 55), (35, 55), (35, 73), (0, 73)]))
+        elif country_code == "NZ":  # New Zealand
+            new_bounds = gpd.GeoSeries(
+                Polygon([(165, -50), (180, -50), (180, -30), (165, -30)])
+            )
+        elif country_code == "PT":  # Portugal
+            new_bounds = gpd.GeoSeries(
+                Polygon([(-10, 35), (0, 35), (0, 45), (-10, 45)])
+            )
+        new_bounds = gpd.GeoDataFrame.from_features(new_bounds, crs=4326)
+
+        # Remove any region outside the new bounds.
+        region_shape = region_shape.overlay(new_bounds, how="intersection")
+
+    return region_shape
+
+
+def get_geopandas_region(country_code: str, make_plot: bool = True) -> gpd.GeoDataFrame:
+    """
+    Get region shape from cartopy, convert it to a geoDataFrame, and plot it.
+
+    Parameters
+    ----------
+    country_code : str
+        The ISO Alpha-2 code of the country of interest
+    make_plot : bool
+        Whether to make a plot of the region of interest
+
+    Returns
+    -------
+    region_shape : geopandas.GeoDataFrame
+        GeoDataFrame containing the region of interest
+    """
+
+    # Load the shapefile containing the world countries.
+    region_shapes = shpreader.natural_earth(
+        resolution="50m", category="cultural", name="admin_0_countries"
+    )
+
+    # Define a reader for the shapefile.
+    reader = shpreader.Reader(region_shapes)
+
+    try:
+        try:
+            # Read the shape of the region of interest by searching for the ISO Alpha-2 code.
+            region_shape = [
+                ii
+                for ii in list(reader.records())
+                if ii.attributes["ISO_A2"] == country_code
+            ][0]
+        except IndexError:
+            # Read the shape of the region of interest by searching for the name of the country.
+            country_name = pycountry.countries.get(alpha_2=country_code).name
+            region_shape = [
+                ii
+                for ii in list(reader.records())
+                if ii.attributes["NAME"] == country_name
+            ][0]
+
+        # Convert the shape to a GeoDataFrame.
+        region_shape = pd.Series({"geometry": region_shape.geometry})
+        region_shape = gpd.GeoSeries(region_shape)
+        region_shape = gpd.GeoDataFrame.from_features(region_shape, crs=4326)
+
+        # Remove small remote islands from the shape of some countries.
+        region_shape = remove_remote_islands(region_shape, country_code)
+
+        # Add the ISO Alpha-2 code as the name of the region.
+        region_shape["name"] = country_code
+        region_shape = region_shape.set_index("name")
+
+        if make_plot:
+            figure_utilities.simple_plot(region_shape, f"region_shape_{country_code}")
+
+    except (AttributeError, IndexError):
+        logging.critical(f"Region shape for {country_code} not found.")
+        region_shape = None
+
+    return region_shape
+
+
+def get_region_bounds(region_shape: gpd.GeoDataFrame) -> list[float]:
+    """
+    Get the lateral bounds of the region of interest including a buffer layer of one degree.
+
+    Parameters
+    ----------
+    region_shape : geopandas.GeoDataFrame
+        GeoDataFrame containing the region of interest
+
+    Returns
+    -------
+    region_bounds : list of float
+        List containing the lateral bounds of the region of interest
+    """
+
+    # Get the lateral bounds of the region of interest including a buffer layer of one degree.
+    region_bounds = (
+        region_shape.union_all().buffer(1).bounds
+    )  # West, South, East, North
+
+    # Round the bounds to the closest 0.25 degree.
+    region_bounds = [round(x * 4) / 4 for x in region_bounds]
+
+    return region_bounds
+
+
+def harmonize_coords(ds: xr.DataArray | xr.Dataset) -> xr.DataArray | xr.Dataset:
+    """
+    Rename coordinates to "x" and "y" and reset longitudes from the range [0, 360] to [-180, 180].
+
+    Parameters
+    ----------
+    ds : xarray.Dataset or xarray.DataArray
+        Dataset or DataArray to be renamed
+
+    Returns
+    -------
+    ds : xarray.Dataset or xarray.DataArray
+        Dataset or DataArray with renamed coordinates and reset longitudes
+    """
+
+    # Rename longitude and latitude coordinates to x and y coordinates.
+    if "longitude" in ds.coords and "latitude" in ds.coords:
+        ds = ds.rename({"longitude": "x", "latitude": "y"})
+    elif "lon" in ds.coords and "lat" in ds.coords:
+        ds = ds.rename({"lon": "x", "lat": "y"})
+
+    # Reset longitudes from the range [0, 360] to [-180, 180].
+    if ds["x"].min() == 0:
+        ds = ds.assign_coords(x=(ds["x"] + 179.99999) % 360 - 179.99999).sortby("x")
+
+    # Sort the dataset by the y coordinate.
+    ds = ds.sortby("y")
+
+    return ds
+
+
+def get_fraction_of_grid_cells_in_shape(
+    region_shape: gpd.GeoDataFrame, resolution: float = 0.25, make_plot: bool = True
+) -> xr.DataArray:
+    """
+    Calculate the fraction of each grid cell that is in the given shape.
+
+    Parameters
+    ----------
+    region_shape : geopandas.GeoDataFrame
+        GeoDataFrame containing the region of interest
+    resolution : float
+        The resolution of the grid cells in degrees
+    make_plot : bool
+        Whether to make a plot of the fraction of each grid cell that is in the given region
+
+    Returns
+    -------
+    fraction_of_grid_cells_in_shape : xarray.DataArray
+        Fraction of each grid cell that is in the given shape
+    """
+
+    # Calculate the lateral bounds for the cutout based on the lateral bounds of the region of interest.
+    cutout_bounds = get_region_bounds(region_shape)
+
+    # Create a temporary cutout to have the grid cell of the region of interest.
+    cutout = atlite.Cutout(
+        "temporary_cutout",
+        module="era5",
+        bounds=cutout_bounds,
+        dx=resolution,
+        dy=resolution,
+        time=slice("2013-01-01", "2013-01-02"),
+    )
+
+    # Calculate the fraction of each grid cell that is in the given shape.
+    fraction_of_grid_cells_in_shape_np = atlite.gis.compute_indicatormatrix(
+        cutout.grid, region_shape, orig_crs=4326, dest_crs=4326
+    ).toarray()
+
+    # Fix NaN and Inf values to 0.0 to avoid numerical issues.
+    fraction_of_grid_cells_in_shape_np = np.nan_to_num(
+        (
+            fraction_of_grid_cells_in_shape_np.T
+            / fraction_of_grid_cells_in_shape_np.sum(axis=1)
+        ).T,
+        nan=0.0,
+        posinf=0.0,
+        neginf=0.0,
+    )
+
+    # Convert the numpy array to a xarray DataArray.
+    fraction_of_grid_cells_in_shape = xr.DataArray(
+        fraction_of_grid_cells_in_shape_np.reshape(
+            len(cutout.data["y"]), len(cutout.data["x"])
+        ),
+        coords={"y": cutout.data["y"], "x": cutout.data["x"]},
+    )
+
+    # Clip the values between 0 and 1.
+    fraction_of_grid_cells_in_shape = (
+        fraction_of_grid_cells_in_shape / fraction_of_grid_cells_in_shape.max()
+    )
+
+    if make_plot:
+        figure_utilities.simple_plot(
+            fraction_of_grid_cells_in_shape,
+            f"fraction_of_grid_cells_in_shape_{region_shape.index[0]}",
+        )
+
+    return fraction_of_grid_cells_in_shape
+
+
+def load_xarray(
+    file_path: str, engine: str = "netcdf4", dataarray_or_dataset: str = "dataarray"
+) -> xr.DataArray | xr.Dataset:
+    """
+    Load an xarray dataset from a file.
+
+    Parameters
+    ----------
+    file_path : str
+        The path to the file to load
+    engine : str
+        The engine to use to load the xarray dataset
+    dataarray_or_dataset : str
+        Whether to load a dataarray or a dataset
+
+    Returns
+    -------
+    xarray_data : xarray.DataArray or xarray.Dataset
+        The loaded dataset
+    """
+
+    # Load the xarray.
+    if dataarray_or_dataset == "dataarray":
+        xarray_data = xr.open_dataarray(file_path, engine=engine)
+    elif dataarray_or_dataset == "dataset":
+        xarray_data = xr.open_dataset(file_path, engine=engine)
+
+    # Harmonize the coordinates of the xarray dataset.
+    xarray_data = harmonize_coords(xarray_data)
+
+    return xarray_data
