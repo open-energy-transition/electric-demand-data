0702c8e007b2788a00b03c3836cbace85eb7344b
eantonini <50218270+eantonini@users.noreply.github.com>
2025-03-07 09:49:47 +0000

    Improved test for time series utilities

--- ETL/util/test_time_series.py
+++ ETL/util/test_time_series.py
@@ -84,11 +84,43 @@ def test_harmonize_time_series(sample_time_series):
 
 
 def test_save_time_series(sample_time_series, tmp_path):
-    # Define the file path where the time series will be saved.
-    file_path = os.path.join(tmp_path, "test_output.csv")
+    # Define the file paths where the time series will be saved.
+    csv_file_path = os.path.join(tmp_path, "test_output.csv")
+    parquet_file_path = os.path.join(tmp_path, "test_output.parquet")
+    unsupported_file_path = os.path.join(tmp_path, "test_output.txt")
 
-    # Save the time series to a csv.
-    save_time_series(sample_time_series, file_path, "TestVariable", local_time_zone)
+    # Save the time series to a csv file by passing a DataFrame.
+    save_time_series(
+        sample_time_series.copy().to_frame(), csv_file_path, "TestVariable"
+    )
+
+    # Save the time series to a csv file using a time zone aware series.
+    save_time_series(sample_time_series.copy(), csv_file_path, "TestVariable")
+
+    # Save the time series to a parquet file using a time zone naive series.
+    save_time_series(
+        sample_time_series.copy().tz_localize(None),
+        parquet_file_path,
+        "TestVariable",
+        local_time_zone,
+    )
+
+    # Save the time series to a csv without time zone information.
+    try:
+        save_time_series(
+            sample_time_series.copy().tz_localize(None), csv_file_path, "TestVariable"
+        )
+    except ValueError:
+        pass
+
+    # Save the time series to an unsupported file format.
+    try:
+        save_time_series(
+            sample_time_series.copy(), unsupported_file_path, "TestVariable"
+        )
+    except ValueError:
+        pass
 
     # Check if the file is created.
-    assert os.path.exists(file_path)
+    assert os.path.exists(csv_file_path)
+    assert os.path.exists(parquet_file_path)
