149d00c26e2567325d076a30bc1c19d3bbbc435f
Vamsipriya22 <golivamsipriya22@gmail.com>
2025-03-19 15:50:52 +0530

    Milestone/gegis replication (#19)
    merge GEGIS branch into demand_asia branch
    
    ---------
    
    Co-authored-by: Kevin Steijn <193539781+ElectricMountains@users.noreply.github.com>
    Co-authored-by: Enrico Antonini <50218270+eantonini@users.noreply.github.com>
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

---
+++ ETL/get_temperature_data.py
@@ -0,0 +1,306 @@
+import logging
+import os
+
+import geopandas as gpd
+import pandas as pd
+import pytz
+import util.general as general_utilities
+import util.geospatial as geospatial_utilities
+import util.time_series as time_series_utilities
+import xarray as xr
+
+
+def get_largest_population_densities_in_region(
+    region_shape: gpd.GeoDataFrame,
+    population_density: xr.DataArray,
+    number_of_grid_cells: int,
+) -> xr.DataArray:
+    """
+    Get the population density data in the given region, ready to be sorted.
+
+    Parameters
+    ----------
+    region_shape : geopandas.GeoDataFrame
+        The shape of the region of interest
+    population_density : xarray.DataArray
+        Population density data
+    number_of_grid_cells : int
+        The number of grid cells to consider
+
+    Returns
+    -------
+    largest_population_densities : xarray.DataArray
+        Population density in the largest population density areas
+    """
+
+    # Calculate the fraction of each grid cell that is in the given shapes.
+    fraction_of_grid_cells_in_shape = (
+        geospatial_utilities.get_fraction_of_grid_cells_in_shape(region_shape)
+    )
+
+    # Rearrange the population density data to sort the values.
+    population_density_rearranged = (
+        population_density.where(fraction_of_grid_cells_in_shape > 0.0)
+        .stack(z=("y", "x"))
+        .dropna(dim="z")
+    )
+
+    # Get the grid cells with the largest population densities.
+    largest_population_densities = population_density_rearranged.sortby(
+        population_density_rearranged
+    ).tail(number_of_grid_cells)
+
+    return largest_population_densities
+
+
+def get_temperature_in_largest_population_density_areas(
+    year: int, region_shape: gpd.GeoDataFrame, number_of_grid_cells: int
+) -> pd.Series:
+    """
+    Get the temperature data for the largest population density areas in the given country.
+
+    Parameters
+    ----------
+    year : int
+        The year of the temperature data
+    region_shape : geopandas.GeoDataFrame
+        The shape of the region of interest
+    number_of_grid_cells : int
+        The number of grid cells to consider
+
+    Returns
+    -------
+    temperature_time_series : pandas.Series
+        Temperature data for the largest population density areas in the given country
+    """
+
+    # Read the temperature data from the Copernicus Climate Data Store (CDS).
+    temperature_data_directory = general_utilities.read_folders_structure()[
+        "weather_folder"
+    ]
+    temperature_data = geospatial_utilities.load_xarray(
+        temperature_data_directory
+        + f"/2m_temperature_{region_shape.index[0]}_{year}.nc"
+    )
+
+    # Read the regional population density data.
+    population_density_directory = general_utilities.read_folders_structure()[
+        "population_density_folder"
+    ]
+    population_density = geospatial_utilities.load_xarray(
+        population_density_directory
+        + f"/population_density_0.25_deg_{region_shape.index[0]}_2015.nc"
+    )
+
+    # Get the population density data in the given region, ready to be sorted.
+    largest_population_densities = get_largest_population_densities_in_region(
+        region_shape, population_density, number_of_grid_cells=number_of_grid_cells
+    )
+
+    # Get the temperature data for the grid cells with the largest population densities.
+    temperature_time_series_in_largest_population_densities = temperature_data.sel(
+        y=largest_population_densities["y"].values,
+        x=largest_population_densities["x"].values,
+    )
+
+    # Calculate the average temperature for the grid cells with the largest population densities.
+    average_temperature_time_series_in_largest_population_densities = (
+        temperature_time_series_in_largest_population_densities.mean(dim=("y", "x"))
+    )
+
+    # Convert the temperature data to a pandas Series.
+    temperature_time_series = (
+        average_temperature_time_series_in_largest_population_densities.to_series()
+    )
+
+    return temperature_time_series
+
+
+def add_temperature_statistics(
+    temperature_time_series: pd.Series, country_time_zone: pytz.timezone
+) -> pd.DataFrame:
+    """
+    Add temperature statistics to the temperature time series.
+
+    Parameters
+    ----------
+    temperature_time_series : pandas.Series
+        Original temperature time series
+    country_time_zone : pytz.timezone
+        Time zone of the country
+
+    Returns
+    -------
+    temperature_time_series : pandas.DataFrame
+        Temperature time series with added statistics
+    """
+
+    # Convert the temperature time series to the local time zone.
+    temperature_time_series = temperature_time_series.tz_localize("UTC").tz_convert(
+        country_time_zone
+    )
+
+    # Get the montly average temperature.
+    monthly_average_temperature = temperature_time_series.resample("ME").mean()
+    monthly_average_temperature.index = monthly_average_temperature.index.month
+
+    # Get the rank of the monthly average temperature.
+    monthly_average_temperature_rank = monthly_average_temperature.rank(ascending=False)
+
+    # Get the annual average temperature.
+    annual_average_temperature = temperature_time_series.resample("YE").mean().values[0]
+
+    # The the 5 and 95 percentiles of the temperature.
+    temperature_5_percentile = temperature_time_series.quantile(0.05)
+    temperature_95_percentile = temperature_time_series.quantile(0.95)
+
+    # Map the monthly average temperature to the original temperature time series.
+    monthly_average_temperature = temperature_time_series.index.month.map(
+        monthly_average_temperature
+    ).to_series()
+    monthly_average_temperature.index = temperature_time_series.index
+
+    # Map the monthly average temperature rank to the original temperature time series.
+    monthly_average_temperature_rank = temperature_time_series.index.month.map(
+        monthly_average_temperature_rank
+    ).to_series()
+    monthly_average_temperature_rank.index = temperature_time_series.index
+
+    # Map the annual average temperature, 5 and 95 percentiles to the original temperature time series.
+    annual_average_temperature = pd.Series(
+        annual_average_temperature, index=temperature_time_series.index
+    )
+    temperature_5_percentile = pd.Series(
+        temperature_5_percentile, index=temperature_time_series.index
+    )
+    temperature_95_percentile = pd.Series(
+        temperature_95_percentile, index=temperature_time_series.index
+    )
+
+    # Add the temperature statistics to the temperature time series.
+    temperature_time_series = temperature_time_series.rename(
+        "Temperature (K)"
+    ).to_frame()
+    temperature_time_series["Monthly average temperature (K)"] = (
+        monthly_average_temperature
+    )
+    temperature_time_series["Monthly average temperature rank"] = (
+        monthly_average_temperature_rank
+    )
+    temperature_time_series["Annual average temperature (K)"] = (
+        annual_average_temperature
+    )
+    temperature_time_series["5 percentile temperature (K)"] = temperature_5_percentile
+    temperature_time_series["95 percentile temperature (K)"] = temperature_95_percentile
+    temperature_time_series.index.name = "Local time"
+
+    return temperature_time_series
+
+
+def run_temperature_calculation() -> None:
+    """
+    Run the calculation of the temperature data according to the population density.
+    """
+
+    # Set up the logging configuration.
+    log_files_directory = general_utilities.read_folders_structure()["log_files_folder"]
+    os.makedirs(log_files_directory, exist_ok=True)
+    log_file_name = "temperature_data.log"
+    logging.basicConfig(
+        filename=log_files_directory + "/" + log_file_name,
+        level=logging.INFO,
+        filemode="w",
+        format="%(asctime)s - %(levelname)s - %(message)s",
+    )
+
+    # Create a directory to store the weather data.
+    result_directory = general_utilities.read_folders_structure()["temperature_folder"]
+    os.makedirs(result_directory, exist_ok=True)
+
+    # Read the codes of the regions of interest.
+    settings_directory = general_utilities.read_folders_structure()["settings_folder"]
+    region_codes = general_utilities.read_codes_from_file(
+        settings_directory + "/gegis__all_countries.yaml"
+    )
+    # region_codes = general_utilities.read_codes_from_file(settings_directory+"/us_eia_regions.yaml")
+
+    # Define the target file type.
+    file_type = ".parquet"
+
+    # Define the years of the data retrieval.
+    start_year = 2015
+    end_year = 2015
+    years = range(start_year, end_year + 1)
+
+    # Loop over the years.
+    for year in years:
+        logging.info(f"Year {year}.")
+
+        # Loop over the regions of interest.
+        for region_code in region_codes:
+            logging.info(f"Retrieving data for {region_code}.")
+
+            # Define the file paths of the temperature time series.
+            temperature_file_path_top_1 = (
+                result_directory
+                + f"/temperature_time_series_top_1_{region_code}_{year}"
+                + file_type
+            )
+            temperature_file_path_top_3 = (
+                result_directory
+                + f"/temperature_time_series_top_3_{region_code}_{year}"
+                + file_type
+            )
+
+            # Check if any of the files does not exist.
+            if not os.path.exists(temperature_file_path_top_1) or not os.path.exists(
+                temperature_file_path_top_3
+            ):
+                # Get the shape of the region of interest.
+                region_shape = geospatial_utilities.get_geopandas_region(region_code)
+
+                # Get the time zone information for the region.
+                region_time_zone = general_utilities.get_time_zone(region_code)
+
+            # Check if the file of temperature time series for the largest population density area does not exist.
+            if not os.path.exists(temperature_file_path_top_1):
+                # Get the temperature data for the largest population density area in the given region.
+                temperature_time_series_top_1 = (
+                    get_temperature_in_largest_population_density_areas(
+                        year, region_shape, number_of_grid_cells=1
+                    )
+                )
+
+                # Add temperature statistics to the time series.
+                temperature_time_series_top_1 = add_temperature_statistics(
+                    temperature_time_series_top_1, region_time_zone
+                )
+
+                # Save the temperature time series.
+                time_series_utilities.save_time_series(
+                    temperature_time_series_top_1,
+                    temperature_file_path_top_1,
+                    temperature_time_series_top_1.columns.values,
+                    local_time_zone=region_time_zone,
+                )
+
+            # Check if the file of temperature time series for the 3 largest population density areas does not exist.
+            if not os.path.exists(temperature_file_path_top_3):
+                # Get the temperature data for the 3 largest population density areas in the given region.
+                temperature_time_series_top_3 = (
+                    get_temperature_in_largest_population_density_areas(
+                        year, region_shape, number_of_grid_cells=3
+                    )
+                )
+
+                # Save the temperature time series.
+                time_series_utilities.save_time_series(
+                    temperature_time_series_top_3,
+                    temperature_file_path_top_3,
+                    "Temperature (K)",
+                    local_time_zone=region_time_zone,
+                )
+
+
+if __name__ == "__main__":
+    run_temperature_calculation()
---
+++ ETL/util/general.py
@@ -0,0 +1,143 @@
+import pycountry
+import pytz
+import yaml
+from countryinfo import CountryInfo
+from timezonefinder import TimezoneFinder
+
+
+def read_folders_structure(file_path: str = "directories.yaml") -> dict[str, str]:
+    """
+    Read the folders structure from a yaml file.
+
+    Parameters
+    ----------
+    file_path : str, optional
+        The path to the yaml file containing the folders structure
+
+    Returns
+    -------
+    folders_structure : dict of str
+        The folders structure
+    """
+
+    # Read the folders structure from the file.
+    with open(file_path, "r") as file:
+        folders_structure = yaml.safe_load(file)
+
+    return folders_structure
+
+
+def read_codes_from_file(file_path: str) -> list[str]:
+    """
+    Read the codes of the countries or regions from a file.
+
+    Parameters
+    ----------
+    file_path : str
+        The path to the file containing the entries
+
+    Returns
+    -------
+    codes : list of str
+        The ISO Alpha-2 codes of the countries or the combination of the ISO Alpha-2 codes and the region codes
+    """
+
+    # Read the countries from the file.
+    with open(file_path, "r") as file:
+        data = yaml.safe_load(file)
+
+    # Extract the entries.
+    items = data["items"]
+
+    # Extract codes.
+    codes = [
+        item["country_code"] + "_" + item["region_code"]
+        if "region_code" in item
+        else item["country_code"]
+        for item in items
+    ]
+
+    return codes
+
+
+def get_us_region_time_zone(region_code: str) -> pytz.timezone:
+    """
+    Get the time zone of a US region.
+
+    Parameters
+    ----------
+    region_code : str
+        The code of the US region
+
+    Returns
+    -------
+    time_zone : pytz.timezone
+        The time zone of the US region
+    """
+
+    # Define the time zones of the US regions.
+    time_zones_mapping = {
+        "US_CAL": "America/Los_Angeles",
+        "US_CAR": "America/New_York",
+        "US_CENT": "America/Chicago",
+        "US_FLA": "America/New_York",
+        "US_MIDA": "America/New_York",
+        "US_MIDW": "America/Chicago",
+        "US_NE": "America/New_York",
+        "US_NY": "America/New_York",
+        "US_NW": "America/Los_Angeles",
+        "US_SE": "America/New_York",
+        "US_SW": "America/Phoenix",
+        "US_TEN": "America/Chicago",
+        "US_TEX": "America/Chicago",
+    }
+
+    return pytz.timezone(time_zones_mapping[region_code])
+
+
+def get_time_zone(code: str) -> pytz.timezone:
+    """
+    Get the time zone of a country.
+
+    Parameters
+    ----------
+    code : str
+        The code of the region (ISO Alpha-2 code or a combination of ISO Alpha-2 code and region code)
+
+    Returns
+    -------
+    time_zone : pytz.timezone
+        The time zone of the region
+    """
+
+    if "_" not in code:
+        # The code is the ISO Alpha-2 code of the country.
+        iso_alpha_2_code = code
+
+        # Get the list of time zones for the country.
+        time_zones = pytz.country_timezones[iso_alpha_2_code]
+
+        # If there are multiple time zones, find the time zone based on the capital city.
+        if len(time_zones) > 1:
+            # Get the country name.
+            country = pycountry.countries.get(alpha_2=iso_alpha_2_code)
+
+            # Get the capital city coordinates.
+            location = CountryInfo(country.name).capital_latlng()
+
+            # Find time zone based on capital city coordinates.
+            tf = TimezoneFinder()
+            time_zone_name = tf.timezone_at(lat=location[0], lng=location[1])
+            time_zone = pytz.timezone(time_zone_name)
+        else:
+            # Get the time zone of the country.
+            time_zone = time_zones[0]
+
+    else:
+        # Split the country code into the ISO Alpha-2 code and the region code.
+        iso_alpha_2_code, region_code = code.split("_")
+
+        if iso_alpha_2_code == "US":
+            time_zone = get_us_region_time_zone(iso_alpha_2_code + "_" + region_code)
+
+    return time_zone
